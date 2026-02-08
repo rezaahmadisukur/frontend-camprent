508d6974a699c2ddf62877710b6cb6b3d4828603
rezaahmadisukur <rezaahmadisukur1309@gmail.com>
2026-02-08 21:31:48 +0700

    feat: add step process section in homepage

---
+++ src/features/home/components/StepProcessSection.tsx
@@ -0,0 +1,49 @@
+import { SearchIcon } from "lucide-react";
+
+const PartProcess = () => {
+  return (
+    <div className="flex flex-col items-center gap-4">
+      {/* Logo */}
+      <div className="w-25 h-25 bg-background rounded-full relative flex justify-center items-center">
+        <SearchIcon className="size-10 text-secondary" />
+        <div className="w-8 h-8 rounded-full absolute bg-primary text-primary-foreground -top-1 -right-1 flex justify-center items-center text-md font-semibold">
+          1
+        </div>
+      </div>
+      {/* Title */}
+      <h1 className="text-foreground text-xl font-semibold">Browse & Select</h1>
+      {/* Description */}
+      <p className="text-foreground/80 text-md font-normal text-center">
+        Explore our curated collection of premium camping gear and add items to
+        your cart.
+      </p>
+    </div>
+  );
+};
+
+const StepProcessSection = () => {
+  return (
+    <section className="max-w-6xl 6xl:container mx-auto">
+      {/* Header */}
+      <div className="max-w-3xl flex flex-col items-center mx-auto gap-4">
+        <p className="w-fit py-2 px-4 bg-secondary/20 text-secondary rounded-full font-semibold text-sm">
+          Simple Process
+        </p>
+        <h1 className="text-4xl font-bold text-foreground">How It Works</h1>
+        <p className="text-foreground/80 text-lg font-normal text-center">
+          Renting camping gear has never been easier. Four simple steps to your
+          next adventure.
+        </p>
+      </div>
+
+      {/* Step by Step */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-20 gap-12">
+        {[1, 2, 3, 4].map((_) => (
+          <PartProcess key={_} />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default StepProcessSection;
